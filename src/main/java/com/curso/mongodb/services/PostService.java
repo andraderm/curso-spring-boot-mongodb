5019c69b8bc6b45f72cd83962d3face32cbde01f
Rodrigo Andrade <rodrigom.andrade95@gmail.com>
2020-06-03 15:29:01 -0300

    Consulta simples com query methods

--- src/main/java/com/curso/mongodb/services/PostService.java
+++ src/main/java/com/curso/mongodb/services/PostService.java
@@ -1,12 +1,12 @@
 package com.curso.mongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.curso.mongodb.domain.Post;
-import com.curso.mongodb.domain.User;
 import com.curso.mongodb.repository.PostRepository;
 import com.curso.mongodb.services.exception.ObjectNotFoundException;
 
@@ -19,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = postRepository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
 	}
 }
 	
